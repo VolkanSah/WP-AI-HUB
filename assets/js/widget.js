8a91c25e5f3e68a0940d153c2d25beb4431aca5f
//NCF //SDCPT <VolkanSah@users.noreply.github.com>
2026-03-13 13:17:32 +0100

    Delete assets/js directory

--- assets/js/widget.js
+++
@@ -1,9 +0,0 @@
-// minimal widget.js
-class AiHubWidget extends AiHubChat {
-    constructor(config) {
-        super(config);
-        this.isOpen = false;
-    }
-    toggle() { ... }
-    //only Widget-spezific
-}
